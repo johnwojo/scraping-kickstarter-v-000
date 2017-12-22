78061c7e97779a1e054b015e2cdd014a6f184b1b
John Wojciechowski <jwojo13@gmail.com>
2017-12-22 18:46:41 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,6 +4,14 @@ require 'nokogiri'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+   projects[project] = {}
+ end
+
+ projects
 end
 
 
